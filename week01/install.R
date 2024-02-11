4eab249067cb55223ffb0bc87857f96d2b34f6b6
Pierre-Luc <pl.germain@gmail.com>
2024-02-11 11:09:41 +0100

    Update install.R

--- week01/install.R
+++ week01/install.R
@@ -1,8 +1,12 @@
-install.packages(c("gsl","ggplot2","remotes"))
+install.packages(c("gsl","ggplot2","remotes","rmarkdown"))
 # If you haven't install Bioconductor yet:
 # install.packages("BiocManager")
 # BiocManager::install()
 
 BiocManager::install(c("GenomicRanges", "rtracklayer", "EnrichedHeatmap", "AnnotationHub", 
-                       "ensembldb", "edgeR", "esATAC", "sechm","genomation","Rsubread","Rfastp"))
+                       "ensembldb", "edgeR", "esATAC", "sechm", "motifmatchr","rGREAT",
+                       "bsseq","DMRcate","data.table","InteractionSet","chromVAR","limma",
+                       "universalmotif", "MotifDb", "TFBSTools", "Biostrings", "PWMEnrich",
+                       "Rsubread","Rfastp"))
+BiocManager::install("Bioconductor/BiocFileCache")
 BiocManager::install("ETHZ-INS/epiwraps")
